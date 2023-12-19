898933922e28d2ba8308b990b4f9a355bf0175e1
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-19 17:21:46 +0530

    Sum all numbers till the given one

--- recursion.js
+++ recursion.js
@@ -7,3 +7,21 @@ function pow(x, n) {
   }
   
   console.log( pow(2, 3) ); // 8
+
+//   task-Sum all numbers till the given one
+// The solution using a loop:
+function sumTo(n){
+    let sum = 0;
+    for(let i=0;i<=n;i++){
+        sum +=i;
+    }
+    return sum;
+}
+console.log(sumTo(100));
+
+// The solution using recursion:
+function sumTo1(n){
+    if(n==1) return n;
+    return n+sumTo1(n-1);
+}
+console.log(sumTo1(100));
