bc3aee1ad6d04a3acff5078224d9dd9c947877b1
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-20 09:38:31 +0530

    Copying and merging objects

--- spresd_Rest.js
+++ spresd_Rest.js
@@ -11,3 +11,17 @@ function sum(x, y, z) {
   console.log(sum.apply(null, numbers));
   // Expected output: 6
   
+//   concat
+let arr1 = [0, 1, 2];
+const arr2 = [3, 4, 5];
+
+// Append all items from arr2 onto arr1
+arr1 = arr1.concat(arr2);
+console.log(arr1);
+
+// Copying and merging objects
+const obj1 = { foo: "bar", x: 42 };
+const obj2 = { bar: "baz", y: 13 };
+
+const mergedObj = { ...obj1, ...obj2 };
+// { foo: "bar", x: 42, bar: "baz", y: 13 }
