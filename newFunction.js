549046e2b46a994993a4938cf00b310e5793267f
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-20 21:49:52 +0530

    way of declartions

--- newFunction.js
+++ newFunction.js
@@ -6,3 +6,8 @@ alert( sum(1, 2) );
 //function without arguments 
 let sayHi = new Function(alert("hello"));
 sayHi();
+
+// way of declartions -all are same
+new Function('a', 'b', 'return a + b'); // basic syntax
+new Function('a,b', 'return a + b'); // comma-separated
+new Function('a , b', 'return a + b'); // comma-separated with spaces
