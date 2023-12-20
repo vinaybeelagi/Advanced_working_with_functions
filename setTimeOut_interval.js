fd8d4feaec3c63d80ee6fc68709e30172421d630
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-20 22:42:02 +0530

    set timeout example with arguments

--- setTimeOut_interval.js
+++ setTimeOut_interval.js
@@ -3,3 +3,9 @@ function sayHi(){
 console.log("hello")
 }
 setTimeout(sayHi,1000);
+
+// With arguments:
+function sayHi(pharse,who){
+    console.log(pharse +' , '+ who);
+}
+setTimeout(sayHi,2000,"Hello","aaru");
