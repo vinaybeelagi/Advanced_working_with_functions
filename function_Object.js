30df9b3fac83e5877e53fcbda1b608e0337f9f61
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-20 20:05:27 +0530

    task-Sum with an arbitrary amount of brackets

--- function_Object.js
+++ function_Object.js
@@ -32,8 +32,8 @@ function sum(a) {
     };  
     return f;
   }  
-  console.log( sum(1)(2) ); // 3
-  console.log( sum(5)(-1)(2) ); // 6
-  console.log( sum(6)(-1)(-2)(-3) ); // 0
-  console.log( sum(0)(1)(2)(3)(4)(5) ); // 15
+  console.log( sum(1)(2).toString() ); // 3
+  console.log( sum(5)(-1)(2).toString() ); // 6
+  console.log( sum(6)(-1)(-2)(-3).toString() ); // 0
+  console.log( sum(0)(1)(2)(3)(4)(5).toString()); // 15
 
