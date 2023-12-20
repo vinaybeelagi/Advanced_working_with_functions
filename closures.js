340c719c23a9c478466416fddd3ae68585da150f
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-20 12:13:39 +0530

    task - function in if

--- closures.js
+++ closures.js
@@ -61,3 +61,18 @@ function counter() {
   alert( counter.up() ); // ans :1
   alert( counter.up() ); // ans :2
   alert( counter.down() ); //ans : 1
+
+//   task-Function in if
+let phrase = "Hello";
+
+if (true) {
+  let user = "John";
+
+  function sayHi() {
+    alert(`${phrase}, ${user}`);
+  }
+}
+
+sayHi();
+//The result is an error.The function sayHi is declared inside the if, so it only lives inside it. There is no sayHi outside.
+
