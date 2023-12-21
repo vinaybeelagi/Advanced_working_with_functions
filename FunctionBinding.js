8fddfae41bcd150e228a9959a23ae22d2378d5b4
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-21 13:34:51 +0530

    Solution 2: bind

--- FunctionBinding.js
+++ FunctionBinding.js
@@ -9,3 +9,13 @@ let user = {
 //     user.sayHi()    
 // }, 1000);
 setTimeout(() => { user.sayHi()}, 1000);
+
+// Solution 2: bind
+let user2 = {
+    firstName:"vinay"
+};
+function func() {
+    console.log(this.firstName);
+}
+let funcUser = func.bind(user2);
+funcUser();
