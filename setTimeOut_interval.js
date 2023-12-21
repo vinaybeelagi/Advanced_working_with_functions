651c787243ae32cda07f33e85b8d68415c29b15f
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-21 10:07:31 +0530

    task-Output every second Using setInterval:

--- setTimeOut_interval.js
+++ setTimeOut_interval.js
@@ -1,37 +1,51 @@
-// set time out example
-function sayHi(){
-console.log("hello")
-}
-setTimeout(sayHi,1000);
+// // set time out example
+// function sayHi(){
+// console.log("hello")
+// }
+// setTimeout(sayHi,1000);
 
-// With arguments:
-function sayHi(pharse,who){
-    console.log(pharse +' , '+ who);
-}
-setTimeout(sayHi,2000,"Hello","aaru");
+// // With arguments:
+// function sayHi(pharse,who){
+//     console.log(pharse +' , '+ who);
+// }
+// setTimeout(sayHi,2000,"Hello","aaru");
 
-let timerId = setTimeout(() => console.log("never happens") , 2000);
-console.log(timerId);
+// let timerId = setTimeout(() => console.log("never happens") , 2000);
+// console.log(timerId);
 
-clearTimeout(timerId);
-console.log(timerId);
+// clearTimeout(timerId);
+// console.log(timerId);
 
-// set interval
-let timerId1 = setInterval(()=> console.log("test"),3000);
-setTimeout(()=>{clearInterval(timerId1)
-console.log("stop"),5000}
-);
+// // set interval
+// let timerId1 = setInterval(()=> console.log("test"),3000);
+// setTimeout(()=>{clearInterval(timerId1)
+// console.log("stop"),5000}
+// );
 
-// task-Output every second (Using nested setTimeout):
+// // task-Output every second (Using nested setTimeout):
+// function printNumbers(from,to) {
+//     let current = from;
+
+//     setTimeout(function go() {
+//         console.log(current);
+//         if (current<to){
+//             setTimeout(go,1000);
+//         }
+//         current++;
+//     },1000)
+// }
+// printNumbers(5,10)
+
+//task-Output every second Using setInterval:
 function printNumbers(from,to) {
     let current = from;
 
-    setTimeout(function go() {
+    let timerId = setInterval(function() {
         console.log(current);
-        if (current<to){
-            setTimeout(go,1000);
+        if(current == to) {
+            clearInterval(timerId);
         }
         current++;
-    },1000)
+    },1000);
 }
-printNumbers(5,10)
+printNumbers(5,10);
