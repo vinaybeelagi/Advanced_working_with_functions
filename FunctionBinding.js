697d02c96deb99985b05bad002acbbacc4bf1e9a
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-21 13:00:54 +0530

    use a wrapping function with arrow fun

--- FunctionBinding.js
+++ FunctionBinding.js
@@ -1,9 +1,11 @@
+// use a wrapping function:
 let user = {
     firstName:"jorge",
     sayHi() {
         console.log(`hello, ${this.firstName}`)
     }
 };
-setTimeout(function(){
-    user.sayHi()    
-}, 1000);
+// setTimeout(function(){
+//     user.sayHi()    
+// }, 1000);
+setTimeout(() => { user.sayHi()}, 1000);
