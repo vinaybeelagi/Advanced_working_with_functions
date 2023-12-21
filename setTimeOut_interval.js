3278d1c16de579c8b257e67f2244e82d7cedd272
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-21 10:12:09 +0530

    task-What will setTimeout show?

--- setTimeOut_interval.js
+++ setTimeOut_interval.js
@@ -49,3 +49,14 @@ function printNumbers(from,to) {
     },1000);
 }
 printNumbers(5,10);
+
+//task-What will setTimeout show?
+let i = 0;
+setTimeout(() => alert(i), 100); // 100000000
+// assume that the time to execute this function is >100ms
+for(let j = 0; j < 100000000; j++) {
+  i++;
+}
+//ANS:Any setTimeout will run only after the current code has finished.
+
+// The i will be the last one: 100000000.
