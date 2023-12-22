4b52b5750aa3eb2b8a5299b10cfad516e6eefc08
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-22 10:07:00 +0530

    To change the flags, we can use Object.defineProperty.

--- Property_Flags_Descriptors.js
+++ Property_Flags_Descriptors.js
@@ -14,3 +14,22 @@ let user = {
     "configurable": true
   }
   */
+
+  //To change the flags, we can use Object.defineProperty.
+  let user = {};
+
+Object.defineProperty(user, "name", {
+  value: "John"
+});
+
+let descriptor = Object.getOwnPropertyDescriptor(user, 'name');
+
+alert( JSON.stringify(descriptor, null, 2 ) );
+/*
+{
+  "value": "John",
+  "writable": false,
+  "enumerable": false,
+  "configurable": false
+}
+ */
