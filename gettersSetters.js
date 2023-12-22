545f6bb9cbc49cdeb7ee3398ad1b59a4bf88ba6c
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-22 11:26:51 +0530

    fix it by adding a setter for user.fullName

--- gettersSetters.js
+++ gettersSetters.js
@@ -7,3 +7,23 @@ let user = {
     }
 }
 console.log(user.fullName);
+
+// Let’s fix it by adding a setter for user.fullName:
+let user2 = {
+    name: "John",
+    surname: "Smith",
+  
+    get fullName() {
+      return `${this.name} ${this.surname}`;
+    },
+  
+    set fullName(value) {
+      [this.name, this.surname] = value.split(" ");
+    }
+  };
+  
+  // set fullName is executed with the given value.
+  user.fullName = "Alice Cooper";
+  
+ console.log(user2.name); // Alice
+ console.log(user2.surname); // Cooper
