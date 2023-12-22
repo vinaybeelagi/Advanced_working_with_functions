2627e1038279b3d50f2b1476d33226f683607743
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-22 10:09:05 +0530

    make user.name non-writable (can’t be reassigned) by changing writable flag

--- Property_Flags_Descriptors.js
+++ Property_Flags_Descriptors.js
@@ -1,35 +1,48 @@
-//Property flags
-let user = {
-    name: "John"
-  };
+// //Property flags
+// let user = {
+//     name: "John"
+//   };
   
-  let descriptor = Object.getOwnPropertyDescriptor(user, 'name');
+//   let descriptor = Object.getOwnPropertyDescriptor(user, 'name');
   
-  alert( JSON.stringify(descriptor, null, 2 ) );
-  /* property descriptor:
-  {
-    "value": "John",
-    "writable": true,
-    "enumerable": true,
-    "configurable": true
-  }
-  */
+//   alert( JSON.stringify(descriptor, null, 2 ) );
+//   /* property descriptor:
+//   {
+//     "value": "John",
+//     "writable": true,
+//     "enumerable": true,
+//     "configurable": true
+//   }
+//   */
 
-  //To change the flags, we can use Object.defineProperty.
-  let user = {};
+//   //To change the flags, we can use Object.defineProperty.
+//   let user2 = {};
+
+// Object.defineProperty(user2, "name", {
+//   value: "John"
+// });
+
+// let descriptor2 = Object.getOwnPropertyDescriptor(user2, 'name');
+
+// alert( JSON.stringify(descriptor2, null, 2 ) );
+// /*
+// {
+//   "value": "John",
+//   "writable": false,
+//   "enumerable": false,
+//   "configurable": false
+// }
+//  */
+
+// make user.name non-writable (can’t be reassigned) by changing writable flag
+let user = { };
 
 Object.defineProperty(user, "name", {
-  value: "John"
+  value: "John",
+  // for new properties we need to explicitly list what's true
+  enumerable: true,
+  configurable: true
 });
 
-let descriptor = Object.getOwnPropertyDescriptor(user, 'name');
-
-alert( JSON.stringify(descriptor, null, 2 ) );
-/*
-{
-  "value": "John",
-  "writable": false,
-  "enumerable": false,
-  "configurable": false
-}
- */
+alert(user.name); // John
+user.name = "Pete"; // Error
